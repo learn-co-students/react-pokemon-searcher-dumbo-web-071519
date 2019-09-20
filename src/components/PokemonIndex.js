e05cff6706bb61d4853c08b1559c5551d9f754a3
Janet Mendez <janetmendez.03@gmail.com>
2019-09-20 15:02:30 -0400

    still have to do form and add pokemon

--- src/components/PokemonIndex.js
+++ src/components/PokemonIndex.js
@@ -5,14 +5,29 @@ import { Search } from 'semantic-ui-react'
 import _ from 'lodash'
 
 class PokemonPage extends React.Component {
+  state ={
+    pokemons: []
+  }
+  handleSearch = (e) => {
+    console.dir(e)
+  }
+  componentDidMount(){
+    fetch('http://localhost:3000/pokemon')
+    .then(res => res.json())
+    .then(data => {
+      this.setState({
+        pokemons: data
+      })
+    })
+  }
   render() {
     return (
       <div>
         <h1>Pokemon Searcher</h1>
         <br />
-        <Search onSearchChange={_.debounce(() => console.log('🤔'), 500)} showNoResults={false} />
+        <Search onSearchChange={_.debounce((e) => this.handleSearch(e), 500)} showNoResults={false} />
         <br />
-        <PokemonCollection />
+        <PokemonCollection pokemons={ this.state.pokemons } />
         <br />
         <PokemonForm />
       </div>
