23c47f6928fb02d0ef197b00ecb92f34650f964a
jason <jasonedlewis@gmail.com>
2019-09-20 21:58:37 -0400

    Done.

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -1,21 +1,39 @@
 import React from 'react'
 import { Card } from 'semantic-ui-react'
+import { stat, Stats } from 'fs'
 
 class PokemonCard extends React.Component {
+
+  state = {
+    clicked:false
+  }
+
+  beenClicked =()=>{
+    this.setState({
+      clicked: !this.state.clicked
+    })
+    
+  }
   render() {
+   
+      const {name, sprites,stats} = this.props.poke
+      // debugger
+      let hp = stats.find(stat => stat.name === 'hp').value
+      
+
     return (
       <Card>
-        <div>
+        <div onClick={()=> this.beenClicked()}>
           <div className="image">
-            <img alt="oh no!" />
+            <img src={this.state.clicked ? sprites.back : sprites.front } alt="oh no!" />
           </div>
           <div className="content">
-            <div className="header">POKEMON NAME HERE</div>
+            <div className="header">{name}</div>
           </div>
           <div className="extra content">
             <span>
               <i className="icon heartbeat red" />
-              POKEMON HP HERE hp
+              {hp} hp
             </span>
           </div>
         </div>
--- src/components/PokemonIndex.js
+++ src/components/PokemonIndex.js
@@ -5,16 +5,34 @@ import { Search } from 'semantic-ui-react'
 import _ from 'lodash'
 
 class PokemonPage extends React.Component {
+
+  state = {
+    pokemonCollection: []
+  }
+
+
+  componentDidMount(){
+
+  fetch('http://localhost:3000/pokemon')
+  .then(resp => resp.json())
+  .then(data => this.setState({
+    pokemonCollection: data
+  }))
+
+
+}
   render() {
     return (
       <div>
         <h1>Pokemon Searcher</h1>
         <br />
-        <Search onSearchChange={_.debounce(() => console.log('🤔'), 500)} showNoResults={false} />
-        <br />
-        <PokemonCollection />
-        <br />
         <PokemonForm />
+        <br />
+        <Search onSearchChange={_.debounce(() => console.log('🤔'), 500)} showNoResults={false} />
+        <br/>
+        <PokemonCollection pokemonCollection={this.state.pokemonCollection} />
+        <br />
+        
       </div>
     )
   }
