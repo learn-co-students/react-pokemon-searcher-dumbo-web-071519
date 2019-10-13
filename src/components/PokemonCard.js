2ec941c840ad69fface17e643402b29557ba0b80
Megan <MeganOliviaWendyAdams@gmail.com>
2019-10-13 18:07:09 -0400

    flip image

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -1,13 +1,32 @@
 import React from 'react'
 import { Card } from 'semantic-ui-react'
 
+
+
+
 class PokemonCard extends React.Component {
+
+
+
+  state = {
+    pokemon_image: this.props.pokemon.sprites.front
+  }
+
+flipImage = () => {
+  if (this.state.pokemon_image === this.props.pokemon.sprites.front)
+  return this.setState({pokemon_image: this.props.pokemon.sprites.back}) 
+  if (this.state.pokemon_image === this.props.pokemon.sprites.back)
+  return this.setState({pokemon_image: this.props.pokemon.sprites.front}) 
+}
+
+
+
   render() {
     return (
-      <Card>
+      <Card onClick={this.flipImage}>
         <div>
           <div className="image">
-            <img src={this.props.pokemon.sprites.front}alt="oh no!" />
+            <img src={this.state.pokemon_image}alt="oh no!" />
           </div>
           <div className="content">
             <div className="header">{this.props.pokemon.name}</div>
@@ -15,7 +34,7 @@ class PokemonCard extends React.Component {
           <div className="extra content">
             <span>
               <i className="icon heartbeat red" />
-              {this.props.pokemon.stats[5].value}
+              {this.props.pokemon.stats[5].hp}
             </span>
           </div>
         </div>
